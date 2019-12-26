25598fa19039a7146f5f09cef00bc17513a07387
songszw <songszw@gmail.com>
2019-12-26 16:37:14 +0800

    完成赠送清单

--- app/models/gift.py
+++ app/models/gift.py
@@ -4,10 +4,11 @@
 @Email   : songszw315@live.com 
 """
 from flask import current_app
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc, func
 from sqlalchemy.orm import relationship
 
 from app.models.base import db, Base
+from app.models.wish import Wish
 from app.spider.yushu_book import YuShuBook
 
 
@@ -17,6 +18,25 @@ class Gift(Base):
     uid = Column(Integer, ForeignKey('user.id'))
     isbn = Column(String(15), nullable=False)
     launched = Column(Boolean, default=False)
+
+    @classmethod
+    def get_user_gifts(cls, uid):
+        gifts = Gift.query.filter_by(uid=uid, launched=False).order_by(
+            desc(Gift.create_time)
+        ).all()
+        return gifts
+
+    @classmethod
+    def get_wish_count(cls, isbn_list):
+        count_list = db.session.query(
+            func.count(Wish.id), Wish.isbn).filter(
+            Wish.launched == False,
+            Wish.isbn.in_(isbn_list),
+            Wish.status == 1).group_by(
+            Wish.isbn).order_by(
+            desc(Wish.create_time)).all()
+        count_list = [{'count': w[0], 'isbn': w[1]} for w in count_list]
+        return count_list
 
     @property
     def book(self):
--- app/web/gift.py
+++ app/web/gift.py
@@ -3,13 +3,19 @@ from flask_login import login_required, current_user
 
 from app.models.base import db
 from app.models.gift import Gift
+from app.view_models.gift import MyGifts
 from . import web
 
 
 @web.route('/my/gifts')
 @login_required
 def my_gifts():
-    return 'MY GIFTS'
+    uid = current_user.id
+    gifts_of_mine = Gift.get_user_gifts(uid)
+    isbn_list = [gift.isbn for gift in gifts_of_mine]
+    wish_count_list = Gift.get_wish_count(isbn_list)
+    view_model = MyGifts(gifts_of_mine, wish_count_list)
+    return render_template('my_gifts.html', gifts=view_model.gifts)
 
 
 @web.route('/gifts/book/<isbn>')
