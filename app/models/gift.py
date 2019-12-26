fc129ef23e13a507dcd910ce5f4117c143a315c7
songszw <songszw@gmail.com>
2019-12-26 14:20:39 +0800

    完成首页,最近的礼物编写

--- app/models/gift.py
+++ app/models/gift.py
@@ -3,10 +3,12 @@
 @Author  : songszw 
 @Email   : songszw315@live.com 
 """
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String
+from flask import current_app
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc
 from sqlalchemy.orm import relationship
 
 from app.models.base import db, Base
+from app.spider.yushu_book import YuShuBook
 
 
 class Gift(Base):
@@ -15,3 +17,25 @@ class Gift(Base):
     uid = Column(Integer, ForeignKey('user.id'))
     isbn = Column(String(15), nullable=False)
     launched = Column(Boolean, default=False)
+
+    @property
+    def book(self):
+        yushu_book = YuShuBook()
+        yushu_book.search_by_isbn(self.isbn)
+        return yushu_book.first
+
+    @classmethod
+    def recent(cls):
+        recent_gift = Gift.query.filter_by(
+            launched=False
+        ).group_by(Gift.isbn).order_by(
+            desc(Gift.create_time)
+        ).limit(
+            current_app.config['RECENT_BOOK_COUNT']
+        ).distinct().all()
+        # recent_gift = Gift.query.filter_by(
+        #     launched=False).group_by(
+        #     Gift.isbn).order_by(
+        #     Gift.create_time).limit(
+        #     current_app.config['RECENT_BOOK_COUNT']).distinct().all()
+        return recent_gift
