399179c8a9dc521df89590405d80ff7adc14f38e
songszw <songszw@gmail.com>
2019-12-27 17:08:00 +0800

    封装gift和wish功能重复代码

--- app/view_models/trade.py
+++ app/view_models/trade.py
@@ -3,6 +3,7 @@
 @Author  : songszw 
 @Email   : songszw315@live.com 
 """
+from app.view_models.book import BookViewModel
 
 
 class TradeInfo:
@@ -25,3 +26,31 @@ class TradeInfo:
             time=time,
             id=single.id
         )
+
+
+class MyTrades:
+    def __init__(self, trades_of_mine, trade_count_list):
+        self.trades = []
+        self.__trades_of_mine = trades_of_mine
+        self.__trade_count_list = trade_count_list
+        self.trades = self.__parse()
+
+    def __parse(self):
+        temp_list = []
+        for trade in self.__trades_of_mine:
+            my_trade = self.__matching(trade)
+            temp_list.append(my_trade)
+        return temp_list
+
+    def __matching(self, trade):
+        count = 0
+        for trade_count in self.__trade_count_list:
+            if trade.isbn == trade_count['isbn']:
+                count = trade_count['count']
+        r = {
+            'wishes_count': count,
+            'book': BookViewModel(trade.book),
+            'id': trade.id
+        }
+        return r
+
